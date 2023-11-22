4d3db84d51f37822f1d5b1cfef4e08fa1a13074a
KHALID ABDU <khalid11abdu@gmail.com>
2023-11-22 14:07:32 +0300

    starting setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactDOM from 'react-dom/client';
 
-// import store from "./store/index_redux";
-import store from "./store/index_toolkit";
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
